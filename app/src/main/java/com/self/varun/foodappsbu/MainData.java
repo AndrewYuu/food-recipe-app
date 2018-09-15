36c4e589828605c130a3ba6a481058c0ff9b336a
Varun Batra <varun.batra@stonybrook.edu>
2018-09-14 22:58:33 -0400

    resolved dependencies for firebase

---
+++ app/src/main/java/com/self/varun/foodappsbu/MainData.java
@@ -0,0 +1,12 @@
+package com.self.varun.foodappsbu;
+
+/**
+ * Created by Varun on 9/14/2018.
+ * Class to pass around data throughout the app
+ */
+public class MainData {
+    String username;
+    String password;
+
+
+}
---
+++ app/src/main/java/com/self/varun/foodappsbu/Recipe.java
@@ -0,0 +1,7 @@
+package com.self.varun.foodappsbu;
+
+/**
+ * Created by Varun on 9/14/2018.
+ */
+public class Recipe {
+}
